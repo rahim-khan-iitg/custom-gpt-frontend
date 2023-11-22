1548fc9598b5f9abd74a1a33f52964eddf1600a2
Rahim Khan <109789890+rahim-khan-iitg@users.noreply.github.com>
2023-11-23 02:29:21 +0530

    some changes

--- utils/bin/sumfetch.js
+++ utils/bin/sumfetch.js
@@ -10,7 +10,7 @@ const sumfetch = async (args) => {
    ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 ▐▓                                 ▐▓       -----------
-▐▓        > CUSTOM LLM             ▐▓        CONTACT 
+▐▓        > CUSTOM GPT             ▐▓        CONTACT 
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/rahim-khan-iitg" target="_blank">github</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://www.linkedin.com/in/rahim-khan-iitg/" target="_blank">linkedin</a></u>
